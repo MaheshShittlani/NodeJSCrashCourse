14ad0673295930f91c4e9e72df1e24d132823b32
Mahesh <mahesh.s@ninehertzindia.com>
2020-10-30 14:33:14 +0530

    Lesson 02

--- Examples/Ch3/ex1/app.js
+++ Examples/Ch3/ex1/app.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((request,response) => {
-    console.log(request.method);
+    console.log("Request receive with http method - "+ request.method);
 });
 
 server.listen(3000, () => {
